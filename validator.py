8be60a44d2a214398d76f645918f22b840a7f250
Cheng Man Li <cmli1@sheffield.ac.uk>
2024-08-22 23:02:23 +0100

    Update error handling logic in validator.py

--- validator.py
+++ validator.py
@@ -1,12 +1,18 @@
 import importlib
+import logging
 
 
-def check_module(module_name: str, form_data: dict):
-    # to check if the specified module exist in the server
+def check_module(module_name, form_data):
+    # Check if the specified module exists in the server
     try:
         module = importlib.import_module(f"modules.{module_name}")
         result = module.validate(**form_data)
         return result
-    except (ImportError, NameError):
-        return False
-
+    except (ImportError, NameError) as e:
+        logging.error("Error importing the specified module")
+        raise e
+    except TypeError as e:
+        logging.error("Error in matching arguments of the module validator")
+        raise e
+    except Exception as e:
+        raise e
